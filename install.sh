e07f26b85e26035ba8a9e7e3b0e19a7a8d2f7708
Xander Vedder <xander.vedd@gmail.com>
2022-06-05 13:49:10 +0200

    Add comment to `install.sh`
    We won't be using the rockspec format. To me, it seems overkill for what
    we are trying to achieve.

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+# Note:
+# I understand that there is a rockspec format for luarocks,
+# but that seems overkill for what I actually need it to do:
+# List dependencies and install them. This is file is a simpler way
+# of doing that.
+
 # Installation directory containing all the modules this project uses.
 DIR_NAME=lua_modules
 
