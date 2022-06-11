d1f1cc7c4309649b27cc0698d19ee4c55150be86
Xander Vedder <xander.vedd@gmail.com>
2022-06-11 12:06:17 +0200

    Purge packages before installing
    Without this, you would have to delete previously installed packages
    manually. This is now done automatically.

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@
 # Installation directory containing all the modules this project uses.
 DIR_NAME=lua_modules
 
+# Before installing, we should clear the tree (if it exists).
+luarocks purge --tree $DIR_NAME
+
 # Install dependencies locally, so LÖVE understands them.
 luarocks install json4lua --tree $DIR_NAME
