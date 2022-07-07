e41794d0045c69bc04abe52cab490cbafb41965f
Xander Vedder <xander.vedd@gmail.com>
2022-07-07 12:07:06 +0200

    Add Location class
    We should also replace all the other location types with the concrete
    class.

---
+++ src/common/location.lua
@@ -0,0 +1,26 @@
+---@class Location
+local Location = {}
+Location.__index = Location
+
+setmetatable(Location, {
+    __call = function(cls, ...)
+        local self = setmetatable({}, cls)
+        self:init(...)
+        return self
+    end
+})
+
+---Constructor
+---@param x number
+---@param y number
+function Location:init(x, y)
+    self.x = x
+    self.y = y
+end
+
+---@param toCompare Location
+function Location:__eq(toCompare)
+    return self.x == toCompare.x and self.y == toCompare.y
+end
+
+return Location
