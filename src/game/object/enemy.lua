xandervedder/TowerYourDefense

commit 8f0d62f01008fe77d54253bb6ebe2977ff574b26

Use algorithm pathing in Enemy
Does not care about obstacles as of yet, that is something entirely
different, but will be used in due time.

src/game/object/enemy.lua
@@ -1,11 +1,20 @@
+local Location = require("src.common.location")
+local Queue = require("src.common.collections.queue")
+
+local Constants = require("src.game.constants")
 local Event = require("src.game.event.event")
 local GameObject = require("src.game.object.gameobject")
 local Publisher = require("src.game.event.publisher")
 local Util = require("src.game.util.util")
 
+--TODO: move to common
+local Size = require("src.gui.style.property.size")
+
 ---@class Enemy : GameObject
 local Enemy = {}
 Enemy.__index = Enemy
+---@type Size
+Enemy.SIZE = Size(16, 16)
 
 setmetatable(Enemy, {
     __index = GameObject,
@@ -16,19 +25,36 @@ setmetatable(Enemy, {
     end
 })
 
-function Enemy:init(o)
+---Constructor
+---@param o table
+---@param parent Spawner
+---@param base Base
+---@param path table<Location>
+function Enemy:init(o, parent, base, path)
     GameObject.init(self, o)
 
-    self.base = o.base
+    ---@type Base
+    self.base = base
+    ---@type Spawner
+    self.parent = parent
+    ---@type Queue
+    self.pathToBase = Queue(path)
+    --? Ignore first location as it's the same as the start (we already know this).
+    self.pathToBase:pop()
+    ---@type Location
+    self.currentLocation = self.pathToBase:pop()
+    ---@type boolean
     self.dead = false
+    ---@type string
     self.direction = ""
+    ---@type number
     self.dmg = 25
+    ---@type number
     self.health = 100
+    ---@type number
     self.originalHealth = self.health
-    self.parent = o.parent
-    -- TODO: fix sizing
-    self.size = { w = 16, h = 16}
-    self.speed = 0.25
+    ---@type number
+    self.speed = 1
 end
 
 function Enemy:draw()
@@ -39,16 +65,16 @@ function Enemy:draw()
         "fill",
         self.position.x,
         self.position.y,
-        self.size.w,
-        self.size.h
+        self.SIZE.w,
+        self.SIZE.h
     )
 
     love.graphics.setColor(1, 0, 0)
     love.graphics.rectangle(
         "fill",
         self.position.x,
-        self.position.y + self.size.h + 5, -- Offset
-        self.size.w,
+        self.position.y + self.SIZE.h + 5, -- Offset
+        self.SIZE.w,
         10
     )
 
@@ -57,28 +83,33 @@ function Enemy:draw()
     love.graphics.rectangle(
         "fill",
         self.position.x,
-        self.position.y + self.size.h + 5, -- Offset
-        self.size.w * percentage,
+        self.position.y + self.SIZE.h + 5, -- Offset
+        self.SIZE.w * percentage,
         10
     )
 end
 
 function Enemy:update()
-    if Util.isWithin(self, self.base) then
+    if Util.isWithinPosition(self.position, self.base:getPosition(), self.base:getSize()) then
         self.base:damage(self.dmg)
         self:die()
         return
     end
 
-    -- TODO: enemies are not moving towards the middle of the base (offset)
-    local bPosition = self.base:getMiddle()
-    if bPosition.x > self.position.x then
+    local currentLocation = self:getCurrentLocation()
+    --TODO: Use Location (or Point in the future).
+    if currentLocation.x == self.position.x and currentLocation.y == self.position.y then
+        self.currentLocation = self.pathToBase:pop()
+        currentLocation = self:getCurrentLocation()
+    end
+
+    if currentLocation.x > self.position.x then
         self.position.x = self.position.x + self.speed
         self.direction = "right"
-    elseif bPosition.x < self.position.x then
+    elseif currentLocation.x < self.position.x then
         self.position.x = self.position.x - self.speed
         self.direction = "left"
-    elseif bPosition.y > self.position.y then
+    elseif currentLocation.y > self.position.y then
         self.position.y = self.position.y + self.speed
         self.direction = "bottom"
     else
@@ -87,11 +118,28 @@ function Enemy:update()
     end
 end
 
+---Gets the current location, but to the middle of it.
+---@return Location
+function Enemy:getCurrentLocation()
+    local x = self.currentLocation.x
+    local y = self.currentLocation.y
+    return Location(
+        (x * Constants.tile.scaledWidth()) + (Constants.tile.scaledWidth() / 2) - (self.SIZE.w / 2),
+        (y * Constants.tile.scaledHeight()) + (Constants.tile.scaledHeight() / 2) - (self.SIZE.h / 2)
+    )
+end
+
 -- TODO: This should be based on the current force... improve:
+---Returns the direction the enemy is currently walking towards.
+---@return string
 function Enemy:getDirection() return self.direction end
 
+---Retrieves the position of the enemy
+---@return table
 function Enemy:getPosition() return GameObject.getMiddle(self) end
 
+---Reduces the health of an enemy by the damage given.
+---@param dmg number
 function Enemy:damage(dmg)
     self.health = self.health - dmg
     if self.health < 0 or self.health == 0 then
@@ -99,6 +147,7 @@ function Enemy:damage(dmg)
     end
 end
 
+---Kills the enemy.
 function Enemy:die()
     self.health = 0
     self.dead = true
@@ -106,6 +155,8 @@ function Enemy:die()
     Publisher.publish(Event("enemy.death", self))
 end
 
+---Returns whether the enemy is dead or not.
+---@return boolean
 function Enemy:isDead()
     return self.dead
 end

src/game/object/spawner.lua
@@ -48,7 +48,9 @@ function Spawner:init(o, base, grid)
         :reconstructPath()
         :get()
 
+    ---@type number
     self.deltaPassed = 0
+    ---@type number
     self.spawnRate = self.spawnRate or 1 -- In seconds
     self.register(self)
     self.enemies = self.getEnemies(self)
@@ -67,6 +69,7 @@ function Spawner:draw()
     love.graphics.setColor(0, 0, 0)
     love.graphics.print("SPAWNER", self.position.x, self.position.y)
 
+    --TODO: add console command for this (see update v0.1.1)
     self:drawDebugPath()
     self:drawSpawnedEnemies()
 end
@@ -110,12 +113,10 @@ end
 function Spawner:spawn()
     local enemy = Enemy({
         position = {
-            x = self.position.x + self.size.w / 2,
-            y = self.position.y + self.size.h / 2
+            x = self.position.x + (self.size.w / 2) - (Enemy.SIZE.w / 2),
+            y = self.position.y + (self.size.h / 2) - (Enemy.SIZE.h / 2)
         },
-        parent = self,
-        base = self.base,
-    })
+    }, self, self.base, self.path)
     table.insert(self.enemies, enemy)
 end
 

commit 144356864932d1f43ab2c948ca8864e056c65299

Fix turret targeting bug
Turned out a simple fix after all, though very annoying and hard to
find this one. This was caused by commit 8f0d62f01008fe77d54253bb6ebe2977ff574b26.

In this commit I added a `SIZE` constant, but I also replaced the `size` property
with nothing. Later in `gameobject.lua` the size is set to a default size (something
with scale).

All that was needed to do to fix this was to add this property back.

src/game/object/enemy.lua
@@ -59,6 +59,8 @@ function Enemy:init(o, parent, base, path)
     self.originalHealth = self.health
     ---@type number
     self.speed = 0.25
+    ---@type Size
+    self.size = self.SIZE
 end
 
 function Enemy:draw()