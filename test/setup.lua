2590b87e56443d28a88a82000c9d4544b105b5b7
Xander Vedder <xander.vedd@gmail.com>
2022-08-12 13:09:40 +0200

    Remove TODO

--- test/setup.lua
+++ test/setup.lua
@@ -1,3 +1,2 @@
--- TODO: A Test Runner would be very nice
 package.path = "lua_modules/share/lua/5.4/?.lua;lua_modules/share/lua/5.4/?/init.lua;" .. package.path
 package.cpath = "lua_modules/lib/lua/5.4/?.so;" .. package.cpath
