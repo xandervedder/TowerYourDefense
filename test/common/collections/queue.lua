fdfd3d9d963edcf67b04cac037f0d9b25d6b198f
Xander Vedder <xander.vedd@gmail.com>
2022-08-12 13:01:58 +0200

    Add tests for `queue.lua`

---
+++ test/common/collections/queue.lua
@@ -0,0 +1,40 @@
+require("test.setup")
+
+local Queue = require("src.common.collections.queue")
+
+local Lu = require("luaunit")
+
+TestQueue = {}
+
+function TestQueue:setUp()
+    ---@type Queue
+    self.queue = Queue()
+end
+
+function TestQueue:testShouldBeEmptyWithEmptyInitialization()
+    Lu.assertTrue(self.queue:empty())
+end
+
+function TestQueue:testShouldNotBeEmptyAfterPushingItemInTheQueue()
+    self.queue:push(1)
+
+    Lu.assertFalse(self.queue:empty())
+end
+
+function TestQueue:testShouldNotBeEmptyAfterPushingTwoInTheQueueAndPoppingOne()
+    self.queue:push(1)
+    self.queue:push(2)
+    local _ = self.queue:pop()
+
+    Lu.assertFalse(self.queue:empty())
+end
+
+function TestQueue:testShouldRecieveNilWhenPoppingAnEmptyQueue()
+    Lu.assertNil(self.queue:pop())
+end
+
+function TestQueue:testShouldRecieveSameItemAfterPushingAndPopping()
+    self.queue:push(1)
+
+    Lu.assertEquals(1, self.queue:pop())
+end
