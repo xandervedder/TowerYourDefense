03c9b72a3386d7a6d2176c5d6a0447dd599e051d
Xander Vedder <xander.vedd@gmail.com>
2022-12-25 12:27:16 +0100

    Add tests for `Timer`

---
+++ test/common/time/timer.lua
@@ -0,0 +1,53 @@
+require("test.setup")
+
+local Timer = require("src.common.time.timer")
+
+local Lu = require("luaunit")
+
+TestTimer = {}
+
+function TestTimer:setup()
+    self.timer = Timer(10)
+end
+
+function TestTimer:testTargetTimeHasPassed()
+    self.timer:update(10)
+    Lu.assertTrue(self.timer:hasPassedTargetSeconds())
+end
+
+function TestTimer:testSecondsHavePassed()
+    self.timer:update(2)
+    Lu.assertTrue(self.timer:hasPassed(2))
+end
+
+function TestTimer:testConsecutiveSecondsHavePassed()
+    self.timer:update(4)
+    Lu.assertTrue(self.timer:hasPassed(2))
+    Lu.assertTrue(self.timer:hasPassed(4))
+
+    self.timer:update(1)
+    Lu.assertFalse(self.timer:hasPassed(2))
+end
+
+function TestTimer:testSecondsHavePassedWithIrregularDelta()
+    self.timer:update(0.5)
+    self.timer:update(0.75)
+    self.timer:update(1.33)
+    Lu.assertTrue(self.timer:hasPassed(2))
+end
+
+function TestTimer:testSecondsGetterWithWholeNumber()
+    self.timer:update(1)
+
+    Lu.assertEquals(self.timer:seconds(), 1)
+end
+
+function TestTimer:testSecondsGetterWithFloatNumber()
+    self.timer:update(1.2)
+
+    Lu.assertEquals(self.timer:seconds(), 1)
+end
+
+function TestTimer:testSecondsGetterWithZero()
+    Lu.assertEquals(self.timer:seconds(), 0)
+end
