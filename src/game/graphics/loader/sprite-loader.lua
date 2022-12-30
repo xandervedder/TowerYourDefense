5a4154346b500e6c90162d56be2b8200dc703e1b
Xander Vedder <xander.vedd@gmail.com>
2022-12-30 15:41:30 +0100

    Prepare `love.Quad[]` in `SpriteLoader`
    For most use cases we won't have to manually define the quads,
    we can get them from the SpriteLoader instead.

--- src/game/graphics/loader/sprite-loader.lua
+++ src/game/graphics/loader/sprite-loader.lua
@@ -1,3 +1,5 @@
+local Constants = require("src.game.constants")
+
 --[[
     SpriteLoader for TowerYourDefense.
     This should be the only place where images are loaded, only loads the images once.
@@ -27,23 +29,61 @@ function SpriteLoader.init()
         collection = self.prepareSprite(self.SPRITE_SHEETS.COLLECTION),
         base = self.prepareSprite(self.SPRITE_SHEETS.TOWER_BASE),
         turret = self.prepareSprite(self.SPRITE_SHEETS.TURRET),
-        mech = self.prepareSprite(self.SPRITE_SHEETS.MECH),
+        mech = self.prepareSprite(self.SPRITE_SHEETS.MECH, 32, 32),
     }
 end
 
 ---@class Sprite
 ---@field imageData love.ImageData
 ---@field image love.Image
+---@field quads love.Quad[]
 
 ---Loads and returns the image with accompanied imageData.
 ---@private
 ---@param location string
+---@param width integer?
+---@param height integer?
 ---@return Sprite
-function SpriteLoader.prepareSprite(location)
+function SpriteLoader.prepareSprite(location, width, height)
+    width = width or Constants.tile.w
+    height = height or Constants.tile.h
     local imageData = love.image.newImageData(location)
     local image = love.graphics.newImage(imageData)
     image:setFilter("nearest", "nearest")
-    return { imageData = imageData, image = image }
+    return {
+        imageData = imageData,
+        image = image,
+        quads = SpriteLoader.prepareQuads(imageData, width, height)
+    }
+end
+
+---Loads all the possible quads that could be in the spritesheet.
+---@private
+---@param imageData love.ImageData
+---@param width integer
+---@param height integer
+---@return love.Quad[]
+function SpriteLoader.prepareQuads(imageData, width, height)
+    local imageWidth, imageHeight = imageData:getDimensions()
+    local imageWidthIndex = imageWidth / width
+    local imageHeightIndex = imageHeight / height
+    local quads = {}
+    for i = 0, imageHeightIndex - 1, 1 do
+        for j = 0, imageWidthIndex - 1, 1 do
+            local quad = love.graphics.newQuad(
+                j * width,
+                i * height,
+                width,
+                height,
+                imageWidth,
+                imageHeight
+            )
+
+            table.insert(quads, quad)
+        end
+    end
+
+    return quads
 end
 
 ---Gets the sprite with the given name.
